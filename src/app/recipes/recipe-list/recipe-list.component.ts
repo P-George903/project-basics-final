9cd4b97f27fb1eb63842195639fe6758ee5e3fca
Peter George <pg3919051@gmail.com>
2020-12-07 14:51:11 -0800

    second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,9 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,18 +11,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
 
-  constructor() { }
+  recipes: Recipe[]
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
   }
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe)
 
-  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import {Recipe} from './recipe.model'
+@Injectable()
+
+
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>()
+    
+    
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Tasty schnitzel', 
+        'This is simply a test', 
+        'https://cdn.pixabay.com/photo/2020/07/01/13/28/wiener-schnitzel-5359641_1280.jpg',
+        [
+            new Ingredient('Meat', 2),
+            new Ingredient('French Fries', 20)
+        ]),
+        
+        new Recipe('Big Fat Burger', 
+        'This is simply a test', 
+        'https://cdn.pixabay.com/photo/2017/04/24/22/12/asparagus-2258013__340.jpg',
+        [
+            new Ingredient("asparagus", 7),
+            new Ingredient("buns", 2)
+        ])
+      ];
+      constructor(private slService: ShoppingListService){
+
+      }
+      getRecipes(){
+          return this.recipes.slice()
+      }
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+            this.slService.addIngredients(ingredients)
+      }
+}
