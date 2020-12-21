e0eec2007c3815bda07a0189a0cb622b8e2b49ba
Peter George <pg3919051@gmail.com>
2020-12-21 12:01:36 -0800

    fourth commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,5 @@
+import {Injectable} from '@angular/core'
 
-import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import {Recipe} from './recipe.model'
@@ -7,7 +7,7 @@ import {Recipe} from './recipe.model'
 
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>()
+  
     
     
     private recipes: Recipe[] = [
